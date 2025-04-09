c90c60e49b3d8afb65ed6b89bb45eeabd23dac32
prasanna260 <114483199+prasanna260@users.noreply.github.com>
2025-04-09 13:07:55 +0530

    Add apps/accounts/admin.py 1744184274398

---
+++ apps/accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('role', 'phone', 'address', 'profile_picture')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
