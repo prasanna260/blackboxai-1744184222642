e89252b0c325de34cd33e7770ad0c5120f99e7e2
prasanna260 <114483199+prasanna260@users.noreply.github.com>
2025-04-09 13:07:53 +0530

    Add apps/accounts/serializers.py 1744184273050

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 
+                 'role', 'phone', 'address', 'profile_picture']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['role'] = user.role
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['user'] = UserSerializer(self.user).data
+        return data
