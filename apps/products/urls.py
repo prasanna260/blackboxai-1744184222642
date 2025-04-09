cd197667926263def5aef2534f50327ddf671d7e
prasanna260 <114483199+prasanna260@users.noreply.github.com>
2025-04-09 22:17:34 +0530

    Add apps/products/urls.py 1744217253707

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
+from .views import ProductListCreateView, ProductDetailView, ProductCategoryView
 
 urlpatterns = [
     path('', ProductListCreateView.as_view(), name='product-list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('category/<str:category>/', ProductCategoryView.as_view(), name='product-category'),
 ]
