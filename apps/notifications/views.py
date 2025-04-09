c52068ee013b0407682b8d5dc1ed23c397dc9ac9
prasanna260 <114483199+prasanna260@users.noreply.github.com>
2025-04-09 13:07:14 +0530

    Add apps/notifications/views.py 1744184233321

---
+++ apps/notifications/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
+from apps.accounts.models import User
+
+class NotificationListView(generics.ListAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
+
+class NotificationUpdateView(generics.UpdateAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
