15894eddd9a5602c542409a9a5afe5b798e0e38f
prasanna260 <114483199+prasanna260@users.noreply.github.com>
2025-04-09 22:17:44 +0530

    Add apps/accounts/urls.py 1744217263639

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, UserProfileView, CustomTokenObtainPairView
+from .views import RegisterView, UserProfileView, CustomTokenObtainPairView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('me/', UserProfileView.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
